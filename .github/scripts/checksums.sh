e19c8ed753daeadc9a00227d0e9a107f245f2fde
kallegrens <k.github@lundgrens.net>
2026-01-24 01:41:13 +0100

    fix: add Linux support for Homebrew installation
    - Consolidate two separate goreleaser configs into single .goreleaser.yaml
    - Use goreleaser-cross container for CGO cross-compilation of all platforms
    - Generate Homebrew Formula (for Linux) in addition to Cask (for macOS)
    - Remove custom checksums script (goreleaser handles this natively)
    - Install X11 dev libraries in workflow for clipboard support
    
    This fixes the issue where Linux users couldn't install via Homebrew:
      Error: attempted to use a Downloadable without a URL!
    
    Linux users can now install with:
      brew tap ramonvermeulen/whosthere && brew install whosthere
    
    macOS users continue to use:
      brew tap ramonvermeulen/whosthere && brew install --cask whosthere

--- .github/scripts/checksums.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-RELEASE_TAG=${GITHUB_REF#refs/tags/}
-mkdir assets
-cd assets
-for url in $(curl -s https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/${RELEASE_TAG} | jq -r '.assets[].browser_download_url'); do
-  curl -L -O "$url"
-done
-sha256sum $(ls | grep -v checksums) > ../whosthere_${RELEASE_TAG}_checksums.txt
-gh release upload ${RELEASE_TAG} ../whosthere_${RELEASE_TAG}_checksums.txt --clobber
